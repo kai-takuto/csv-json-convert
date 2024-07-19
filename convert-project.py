d419befcf925cdf0fcb895164c6e6be829f51fd3
kaitkt <kaitkt@inap-vision.co.jp>
2024-07-19 15:54:13 +0900

    import jsonの削除

--- convert-project.py
+++ convert-project.py
@@ -1,6 +1,4 @@
 import csv
-import json
-
 # csvファイルを読み込む工程
 with open('input.csv', mode='r', encoding='utf-8') as f:
     reader = csv.DictReader(f)
@@ -9,4 +7,3 @@ with open('input.csv', mode='r', encoding='utf-8') as f:
     # データを抜き取った後に、csv_file_dateに追加する
     for raw in reader:
         csv_file_date.append(raw)
-
