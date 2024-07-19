fa0059f017f6e88733818510f904d5d99738d51f
kaitkt <kaitkt@inap-vision.co.jp>
2024-07-19 15:29:41 +0900

    インデントの変更

--- convert-project.py
+++ convert-project.py
@@ -4,8 +4,9 @@ import json
 # csvファイルを読み込む工程
 with open('input.csv', mode='r', encoding='utf-8') as f:
     reader = csv.DictReader(f)
-# 読み込んだデータを格納する
-csv_file_date: list[str] = []
-# データを抜き取った後に、csv_file_dateに追加する
-for raw in reader:
-    csv_file_date.append(raw)
+    # 読み込んだデータを格納する
+    csv_file_date: list[str] = []
+    # データを抜き取った後に、csv_file_dateに追加する
+    for raw in reader:
+        csv_file_date.append(raw)
+
