c28277f913f981e4cb09b8cb5bcbffe277e9918b
kaitkt <kaitkt@inap-vision.co.jp>
2024-07-22 14:42:27 +0900

    Generatorの追加

--- convert-project.py
+++ convert-project.py
@@ -27,4 +27,5 @@ def read_csv_to_list(input_file: str) -> list:
         # データを抜き取った後に、csv_file_dateに追加する
         for raw in reader:
             csv_file_date.append(raw)
+            yield raw
     return csv_file_date
