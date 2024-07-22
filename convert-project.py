7db68ca0008b6d6f1bce1b13522f08047c475f19
kaitkt <kaitkt@inap-vision.co.jp>
2024-07-22 10:44:52 +0900

    globalスコープからlocalスコープに変更

--- convert-project.py
+++ convert-project.py
@@ -1,9 +1,13 @@
 import csv
+
+
 # csvファイルを読み込む工程
-with open('input.csv', mode='r', encoding='utf-8') as f:
-    reader = csv.DictReader(f)
+def read_csv_to_list(input_file: str) -> list:
     # 読み込んだデータを格納する
     csv_file_date: list[str] = []
-    # データを抜き取った後に、csv_file_dateに追加する
-    for raw in reader:
-        csv_file_date.append(raw)
+    with open(input_file, mode='r', newline='', encoding='utf-8') as csvfile:
+        reader: [str] = csv.DictReader(csvfile)
+        # データを抜き取った後に、csv_file_dateに追加する
+        for raw in reader:
+            csv_file_date.append(raw)
+    return csv_file_date
