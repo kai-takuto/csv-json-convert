ab25fb6a1b695432e90e2d892d53ba36b2958ca6
kaitkt <kaitkt@inap-vision.co.jp>
2024-07-23 14:37:39 +0900

    追加した関数に合わせた変数名に変更

--- convert-project.py
+++ convert-project.py
@@ -3,26 +3,26 @@ import os
 import json
 
 
-def check_file_exist(csv_file: str) -> bool:
+def check_file_exist(file_path: str) -> bool:
     """
     入力したファイルが自分のPCに入っているのかを確認する関数
-    :param csv_file: jsonのデータ形式に変換したいファイル
+    :param file_path: jsonのデータ形式に変換したいファイル
     :return: ファイルが存在していればTrue、存在していなかったらFalseを返す
     """
-    if not os.path.exists(csv_file):
+    if not os.path.exists(file_path):
         print('Error : Input file does not exist.')
         return False
     return True
 
 
-def read_csv_to_list(input_file: str) -> list:
+def read_csv_to_list(csv_file: str) -> list:
     """
     csvファイルを読み込み、jsonファイルに書き込むためのデータを格納する関数
-    :param input_file: jsonのデータ形式に変換したいファイル
+    :param csv_file: jsonのデータ形式に変換したいファイル
     :return: input_fileのデータを格納したリスト
     """
     # 読み込んだデータを処理
-    with open(input_file, mode='r', newline='', encoding='utf-8') as csvfile:
+    with open(csv_file, mode='r', newline='', encoding='utf-8') as csvfile:
         rows: csv.DictReader = csv.DictReader(csvfile)
         # データを抜き取った後に、csv_file_dateに追加する
         for row in rows:
